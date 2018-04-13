edb9babecb4b43fac9f6a54a5c7a94577a858d3e
Samuel Nelson <samuel@spring.co.nz>
2018-04-13 14:41:08 +1200

    Reproducing bug with metro resolving React relative to RN

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Button, Text } from 'react-native'
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <View>
+                <Button><Text>Foo</Text></Button>
+            </View>
+        )
+    }
+}
